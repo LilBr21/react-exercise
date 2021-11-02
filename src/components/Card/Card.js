79dd775b38e137fd43e4718af618f18b17f33e12
Halina <halina.syposz@gmail.com>
2021-11-02 17:15:04 +0100

    rwd implemented

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import './card.styles.scss';
 
 const Card = ({
@@ -9,6 +11,27 @@ bronze,
 silver,
 gold
 }) => {
+    const [bronzeMedalIsShown, setBronzeMedalIsShown] = useState(true);
+    const [silverMedalIsShown, setSilverMedalIsShown] = useState(true);
+    const [goldMedalIsShown, setGoldMedalIsShown] = useState(true);
+
+    let checkIfHasMedals = (bronze, silver, gold) => {
+        if (bronze === 0) {
+            setBronzeMedalIsShown(false);
+        }
+        if (silver === 0) {
+            setSilverMedalIsShown(false);
+        }
+        if (gold === 0) {
+            setGoldMedalIsShown(false);
+        }
+    }
+
+    useEffect(() => {
+        checkIfHasMedals(bronze, silver, gold);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
      return (
          <div className="card-container">
              <div className="card-top-part" style={{backgroundImage: `url(${background})`}}>
@@ -21,18 +44,21 @@ gold
              <div className="card-bottom-part">
                 <h2>Rewards</h2>
                 <div className="medals-container">
+                    {bronzeMedalIsShown && 
                     <div className="card-medals">
                         <p className="medal-quant">{bronze}</p>
                         <p className="medal-type">Bronze</p>
-                    </div>
+                    </div>}
+                    {silverMedalIsShown &&
                     <div className="card-medals">
                         <p className="medal-quant">{silver}</p>
                         <p className="medal-type">Silver</p>
-                    </div>
+                    </div>}
+                    {goldMedalIsShown &&
                     <div className="card-medals">
                         <p className="medal-quant">{gold}</p>
                         <p className="medal-type">Gold</p>
-                    </div>
+                    </div>}
                 </div>
              </div>
          </div>
